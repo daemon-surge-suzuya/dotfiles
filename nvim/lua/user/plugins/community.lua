0d9aa8db1c8ba17f0129bf72fb60ca1e95c65b83
daemon-surge-suzuya <daemonsurges@gmail.com>
2023-10-31 14:00:50 +0300

    Had to modify nvim again

---
+++ nvim/lua/user/plugins/community.lua
@@ -0,0 +1,20 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+  { import = "astrocommunity.git/git-blame-nvim" },
+  { import = "astrocommunity.utility/noice-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim"},
+  { import = "astrocommunity.colorscheme.kanagawa-nvim"},
+  { import = "astrocommunity.colorscheme.rose-pine" },
+
+
+  -- Customise opts of some community / core plugins
+  {
+    "noice.nvim",
+    opts = function(_, opts)
+      opts.presets.bottom_search = false
+      opts.lsp.signature = { enabled = false }
+      return opts
+    end,
+  },
+}
